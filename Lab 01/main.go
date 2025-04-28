8b3036e9ced6d151cdbf4e008cb6ba1a262dd281
Kelnit <kelpintech@gmail.com>
2025-04-28 20:05:01 +0700

    Belajar Golang

---
+++ Lab 01/main.go
@@ -0,0 +1,12 @@
+/*
+Lab 01 : Hello, Golang !
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	// Hello World !
+	fmt.Println("Hello, Golang !")
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Ur
+Ur : Belajar Golang !
+
+## 💻 Ontech
+[![Our Technologies](https://skillicons.dev/icons?i=golang)](https://skillicons.dev)
